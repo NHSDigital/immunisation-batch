ae221c2f392e12bdacc0cb32e447cabd8dd6b2c8
CLJ2006 <clarissa.john1@nhs.net>
2024-07-11 17:01:20 +0100

    lint

--- batch/src/ods_patterns.py
+++ batch/src/ods_patterns.py
@@ -1,8 +1,8 @@
 ODS_PATTERNS = {'YGM41': 'EMIS',
                 '8J1100001': 'Pinnacle',
-                 '8HK48': 'Sonar',
-                 'YGA': 'TPP',
-                 '0DE': 'AGEM-NIVS',
-                 '0DF': 'NIMS',
-                 '8HA94': 'EVA',
-                 'X26': 'RAVS'}
+                '8HK48': 'Sonar',
+                'YGA': 'TPP',
+                '0DE': 'AGEM-NIVS',
+                '0DF': 'NIMS',
+                '8HA94': 'EVA',
+                'X26': 'RAVS'}
