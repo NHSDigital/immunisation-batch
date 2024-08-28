169a60d2a598ea2a56d0695407164801b1cf1af6
AlexandraBenson <alexandra.benson3@nhs.net>
2024-08-28 16:38:46 +0100

    add BatchSupplierSystem header to API requests

--- recordforwarder/src/immunisation_api.py
+++ recordforwarder/src/immunisation_api.py
@@ -17,57 +17,38 @@ class ImmunizationApi:
             else "https://api.service.nhs.uk/immunisation-fhir-api"
         )
 
-    def create_immunization(self, imms):
-        return self._send(
-            "POST",
-            "/Immunization",
-            imms,
-            None
+    def create_immunization(self, imms, supplier_system):
+        return self._send("POST", "/Immunization", imms, None, supplier_system)
 
-        )
-
-    def update_immunization(self, imms_id, version_id, imms):
+    def update_immunization(self, imms_id, version_id, imms, supplier_system):
         print(f"imms_id:{imms_id}")
         print(f"version_id:{version_id}")
-        return self._send(
-            "PUT",
-            f"/Immunization/{imms_id}",
-            imms,
-            version_id
-        )
+        return self._send("PUT", f"/Immunization/{imms_id}", imms, version_id, supplier_system)
 
-    def delete_immunization(self, imms_id, imms):
+    def delete_immunization(self, imms_id, imms, supplier_system):
         print(f"imms_id:{imms_id}")
-        return self._send(
-            "DELETE",
-            f"/Immunization/{imms_id}",
-            imms,
-            None
-        )
+        return self._send("DELETE", f"/Immunization/{imms_id}", imms, None, supplier_system)
 
-    def _send(self, method: str, path: str, imms, version_id):
+    def _send(self, method: str, path: str, imms, version_id, supplier_system):
         print("send_started")
         print(f"version_id:{version_id}")
         access_token = self.authenticator.get_access_token()
         logger.debug(f"Access token obtained: {access_token}")
         print(f"access_token:{access_token}")
         request_headers = {
-            'Authorization': f'Bearer {access_token}',
-            'X-Request-ID': str(uuid.uuid4()),
-            'X-Correlation-ID': str(uuid.uuid4()),
+            "Authorization": f"Bearer {access_token}",
+            "X-Request-ID": str(uuid.uuid4()),
+            "X-Correlation-ID": str(uuid.uuid4()),
             "Content-Type": "application/fhir+json",
             "Accept": "application/fhir+json",
+            "BatchSupplierSystem": supplier_system,
         }
         # Conditionally add the "E-Tag" header if version_id is present
         if version_id:
             request_headers["E-Tag"] = str(version_id)
         print(f"request_headers:{request_headers}")
         response = requests.request(
-            method=method,
-            url=f"{self.base_url}/{path}",
-            json=imms,
-            headers=request_headers,
-            timeout=5
+            method=method, url=f"{self.base_url}/{path}", json=imms, headers=request_headers, timeout=5
         )
         print(f"response: {response}")
         print(f"response_json: {response.text}")
---
+++ recordforwarder/src/ods_patterns.py
@@ -0,0 +1,19 @@
+"""Dictionary mapping supplier ODS CODEs to the supplier name"""
+
+ODS_PATTERNS = {
+    "YGM41": "EMIS",
+    "8J1100001": "PINNACLE",
+    "8HK48": "SONAR",
+    "YGA": "TPP",
+    "0DE": "AGEM-NIVS",
+    "0DF": "NIMS",
+    "8HA94": "EVA",
+    "X26": "RAVS",
+    "YGMYH": "MEDICAL_DIRECTOR",
+    "W00": "WELSH_DA_1",
+    "W000": "WELSH_DA_2",
+    "ZT001": "NORTHERN_IRELAND_DA",
+    "YA7": "SCOTLAND_DA",
+    "N2N9I": "COVID19_VACCINE_RESOLUTION_SERVICEDESK",
+    "YGJ": "EMIS",
+}
