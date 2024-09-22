1dd93ff30081bbe6f046c5f2921630261625f0d6
AlexandraBenson <alexandra.benson3@nhs.net>
2024-09-22 15:30:43 +0100

    Remove unneeded test and code

--- filenameprocessor/tests/test_initial_file_validation.py
+++ filenameprocessor/tests/test_initial_file_validation.py
@@ -31,16 +31,6 @@ class TestInitialFileValidation(unittest.TestCase):
         self.test_file_key = "Flu_Vaccinations_v5_DPSFULL_20200101T12345600.csv"
         self.test_bucket_name = "test_bucket"
 
-    def setup_s3_bucket_and_file(self, test_bucket_name, test_file_key, test_file_content=Constants.valid_file_content):
-        """
-        Sets up the S3 client and uploads the test file, containing the test file content, to a bucket named
-        'test_bucket'. Returns the S3 client
-        """
-        s3_client = boto3.client("s3", region_name="eu-west-2")
-        s3_client.create_bucket(Bucket=test_bucket_name, CreateBucketConfiguration={"LocationConstraint": "eu-west-2"})
-        s3_client.put_object(Bucket=test_bucket_name, Key=test_file_key, Body=test_file_content)
-        return s3_client
-
     def test_is_valid_datetime(self):
         "Tests that is_valid_datetime returns True for valid datetimes, and false otherwise"
         test_cases = {
--- filenameprocessor/tests/test_router_lambda_function.py
+++ filenameprocessor/tests/test_router_lambda_function.py
@@ -1,9 +1,6 @@
-import unittest
 import os
 import io
 import csv
-from unittest.mock import patch, MagicMock
-from make_and_upload_ack_file import make_and_upload_ack_file
 
 
 def convert_csv_to_string(filename):
@@ -21,21 +18,3 @@ def convert_csv_to_reader(filename):
 
 def convert_string_to_dict_reader(data_string: str):
     return csv.DictReader(io.StringIO(data_string), delimiter="|")
-
-
-class TestRouterLambdaFunctions(unittest.TestCase):
-    def setUp(self):
-        self.file_key = "Flu_Vaccinations_v5_YGM41_20240708T12130100.csv"
-        self.bucket_name = "test-bucket"
-        self.ods_code = "YGM41"
-        self.mock_s3_client = MagicMock()
-        self.mock_sqs_client = MagicMock()
-
-    def test_make_and_upload_ack_file(self):
-        """tests whether ack file is created"""
-        validation_passed = True
-        created_at_formatted = "20240725T12510700"
-        s3_client = MagicMock()
-        with patch("make_and_upload_ack_file.s3_client", s3_client):
-            make_and_upload_ack_file("1", self.file_key, validation_passed, True, created_at_formatted)
-        s3_client.upload_fileobj.assert_called_once()
