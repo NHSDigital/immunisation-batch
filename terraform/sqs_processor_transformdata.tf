083207cd2226b66fd197047af6125d48ea5492a5
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-09-23 12:30:37 +0100

    Added new kinesis arn in local

--- terraform/sqs_processor_transformdata.tf
+++ terraform/sqs_processor_transformdata.tf
@@ -3,4 +3,6 @@ resource "aws_kinesis_stream" "processor_data_streams" {
   name        = "${local.short_queue_prefix}-processingdata-stream"
   shard_count = 15  
 }
-
+locals {
+  new_kinesis_arns = aws_kinesis_stream.processor_data_streams.name
+}
