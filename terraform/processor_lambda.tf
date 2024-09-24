e329c1e79d60d629ee711de1c8d096d4f520450a
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-09-24 14:44:32 +0100

    Committed  ecs service config

--- terraform/processor_lambda.tf
+++ terraform/processor_lambda.tf
@@ -192,18 +192,18 @@ resource "aws_ecs_task_definition" "ecs_task" {
 }
 
 # Define the ECS Service to Run the Task Definition
-resource "aws_ecs_service" "ecs_service" {
-  name            = "${local.prefix}-processor-service"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.ecs_task.arn
-  desired_count   = 0
-  launch_type     = "FARGATE"
-  force_new_deployment = true
-  network_configuration {
-            subnets          = data.aws_subnets.default.ids
-            assign_public_ip = true
-        }
-}
+# resource "aws_ecs_service" "ecs_service" {
+#   name            = "${local.prefix}-processor-service"
+#   cluster         = aws_ecs_cluster.ecs_cluster.id
+#   task_definition = aws_ecs_task_definition.ecs_task.arn
+#   desired_count   = 0
+#   launch_type     = "FARGATE"
+#   force_new_deployment = true
+#   network_configuration {
+#             subnets          = data.aws_subnets.default.ids
+#             assign_public_ip = true
+#         }
+# }
 
 # IAM Role for EventBridge Pipe
 resource "aws_iam_role" "fifo_pipe_role" {
