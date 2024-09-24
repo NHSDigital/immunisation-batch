8c24e1593279948be918ad38056c3a876a983699
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-09-24 14:01:12 +0100

    Added print statement

--- recordprocessor/src/processing_lambda.py
+++ recordprocessor/src/processing_lambda.py
@@ -250,8 +250,8 @@ def process_lambda_handler(event, context):
     # config_bucket_name = os.getenv("CONFIG_BUCKET_NAME", f"immunisation-batch-{imms_env}-config")
 
     try:
-        logger.info("started")
-        logger.info(f"event:{event}")
+        print("started")
+        print(f"event:{event}")
         # message_id = message_body.get("message_id")
         # vaccine_type = message_body.get("vaccine_type")
         # supplier = message_body.get("supplier")
