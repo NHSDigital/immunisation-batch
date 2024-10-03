ce4d0890ad660d528b62473d801f27682f3fb21e
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-10-03 22:02:28 +0100

    added the code

--- terraform/file_name_processor.tf
+++ terraform/file_name_processor.tf
@@ -222,15 +222,16 @@ resource "aws_security_group_rule" "redis_egress" {
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "10.0.1.0/24" 
-  availability_zone = "eu-west-2a"  
+  cidr_block        = "172.31.1.0/24"
+  availability_zone = "eu-west-2a"
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-west-2b" 
+  cidr_block        = "172.31.2.0/24"
+  availability_zone = "eu-west-2b"
 }
+
 
 
 resource "aws_elasticache_cluster" "redis_cluster" {
