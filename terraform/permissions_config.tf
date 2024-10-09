56b3b7708efd6c763cc6026325cc207eb9b5fb28
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-10-09 12:35:02 +0100

    removed confif policy

--- terraform/permissions_config.tf
+++ terraform/permissions_config.tf
@@ -2,27 +2,3 @@ resource "aws_s3_bucket" "batch_config_bucket" {
   bucket        = "${local.prefix}-configs"
   force_destroy = local.is_temp
 }
-
-resource "aws_s3_bucket_policy" "batch_config_bucket_policy" {
-  bucket = aws_s3_bucket.batch_config_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = aws_iam_role.lambda_exec_role.arn
-        },
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          "arn:aws:s3:::${local.prefix}-configs",
-          "arn:aws:s3:::${local.prefix}-configs/*"
-        ]
-      }
-    ]
-  })
-}
