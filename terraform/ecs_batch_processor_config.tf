a8b6719fbde47317a16aaacbca91072c12eb6ea1
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-10-10 16:09:24 +0100

    Few pipeline changes

--- terraform/ecs_batch_processor_config.tf
+++ terraform/ecs_batch_processor_config.tf
@@ -31,11 +31,11 @@ module "processing_docker_image" {
     "rules" : [
       {
         "rulePriority" : 1,
-        "description" : "Keep only the last 1 images",
+        "description" : "Keep only the last 2 images",
         "selection" : {
           "tagStatus" : "any",
           "countType" : "imageCountMoreThan",
-          "countNumber" : 1
+          "countNumber" : 2
         },
         "action" : {
           "type" : "expire"
