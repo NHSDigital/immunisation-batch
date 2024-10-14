2da1e5fe8002c7c660bcf33d53236b0d99948a46
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-10-14 17:18:43 +0100

    few changes

--- terraform/rediscache.tf
+++ terraform/rediscache.tf
@@ -6,7 +6,7 @@ resource "aws_elasticache_cluster" "redis_cluster" {
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
   port                 = 6379
-  security_group_ids   = [aws_security_group.redis_sg.id]
+  security_group_ids   = [aws_security_group.lambda_redis_sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 }
 
