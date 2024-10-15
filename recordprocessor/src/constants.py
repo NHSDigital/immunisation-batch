31f6cf305093e60b1e9ed4fdd7059143d8cf5896
AlexandraBenson <alexandra.benson3@nhs.net>
2024-10-15 13:16:22 +0100

    Update ack file headers

--- recordprocessor/src/constants.py
+++ recordprocessor/src/constants.py
@@ -9,12 +9,15 @@ class Constants:
         "HEADER_RESPONSE_CODE",
         "ISSUE_SEVERITY",
         "ISSUE_CODE",
+        "ISSUE_DETAILS_CODE",
         "RESPONSE_TYPE",
         "RESPONSE_CODE",
         "RESPONSE_DISPLAY",
         "RECEIVED_TIME",
         "MAILBOX_FROM",
         "LOCAL_ID",
+        "IMMS_ID",
+        "OPERATION_OUTCOME",
         "MESSAGE_DELIVERY",
     ]
 
