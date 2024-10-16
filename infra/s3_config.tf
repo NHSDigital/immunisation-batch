e0918c0fba49f3aa9f7c76dfda2535e60c995a48
ASubaran <akra2@hscic.gov.uk>
2024-10-16 11:58:44 +0100

    added public access block for config bucket

--- infra/s3_config.tf
+++ infra/s3_config.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "batch_config_bucket" {
     bucket        = "imms-internal-dev-supplier-config"
 }
+
+resource "aws_s3_bucket_public_access_block" "batch_config_bucket-public-access-block" {
+  bucket = aws_s3_bucket.batch_config_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
