a189ceab07f27065b5e6e96debd092320c911763
AlexandraBenson <alexandra.benson3@nhs.net>
2024-10-29 12:14:29 +0000

    Fix terraform issue

--- terraform/kinesis_config.tf
+++ terraform/kinesis_config.tf
@@ -1,9 +1,9 @@
 # Define the Kinesis Data Stream resource with 15 shards
-stream_mode_details {
-    stream_mode = "ON_DEMAND"
-}
 resource "aws_kinesis_stream" "processor_data_streams" {
   name        = "${local.short_queue_prefix}-processingdata-stream"
+  stream_mode_details {
+    stream_mode = "ON_DEMAND"
+  }
 }
 locals {
   new_kinesis_arn = aws_kinesis_stream.processor_data_streams.arn
