16c5b5991adcdc2aa586b547aa7dbc1e8f02d3c5
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-10-29 10:13:44 +0000

    added  code

--- terraform/kinesis_config.tf
+++ terraform/kinesis_config.tf
@@ -1,7 +1,11 @@
 # Define the Kinesis Data Stream resource with 15 shards
 resource "aws_kinesis_stream" "processor_data_streams" {
   name        = "${local.short_queue_prefix}-processingdata-stream"
+  stream_mode_details {
+    stream_mode = "ON_DEMAND"
+  }
 }
 locals {
   new_kinesis_arn = aws_kinesis_stream.processor_data_streams.arn
 }
+
