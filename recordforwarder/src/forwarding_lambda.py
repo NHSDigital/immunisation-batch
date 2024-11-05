b5170b82b9546fafa824e5eebf4c5257daebaa33
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-11-05 19:10:06 +0000

    Removed ACK for success

--- recordforwarder/src/forwarding_lambda.py
+++ recordforwarder/src/forwarding_lambda.py
@@ -17,8 +17,8 @@ def forward_request_to_lambda(message_body):
     row_id = message_body.get("row_id")
     logger.info("BEGINNIING FORWARDING MESSAGE: ID %s", row_id)
     try:
-        imms_id = send_request_to_lambda(message_body)
-        update_ack_file(file_key, row_id, successful_api_response=True, diagnostics=None, imms_id=imms_id)
+        send_request_to_lambda(message_body)
+        # update_ack_file(file_key, row_id, successful_api_response=True, diagnostics=None, imms_id=imms_id)
     except MessageNotSuccessfulError as error:
         update_ack_file(file_key, row_id, successful_api_response=False, diagnostics=str(error.message), imms_id=None)
     logger.info("FINISHED FORWARDING MESSAGE: ID %s", row_id)
