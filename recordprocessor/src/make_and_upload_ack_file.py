8a8b1e363a5f8c146399e3dfc8570da7c96eb50d
CLJ2006 <clarissa.john1@nhs.net>
2024-11-22 14:53:12 +0000

    updating times

--- recordprocessor/src/make_and_upload_ack_file.py
+++ recordprocessor/src/make_and_upload_ack_file.py
@@ -33,7 +33,7 @@ def make_ack_data(
 
 def upload_ack_file(file_key: str, ack_data: dict) -> None:
     """Formats the ack data into a csv file and uploads it to the ack bucket"""
-    ack_file_timestamp = datetime.now().replace(second=0, microsecond=0).isoformat(timespec="milliseconds")
+    ack_file_timestamp = datetime.now().isoformat(timespec="milliseconds")
     ack_filename = "ack/" + file_key.replace(".csv", f"_InfAck_{ack_file_timestamp}.csv")
     # Create CSV file with | delimiter, filetype .csv
     csv_buffer = StringIO()
